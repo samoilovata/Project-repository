78d73d9a09c842d738e957246ff32a608bbd4185
Cccuriosity <levshin.iliya@yandex.ru>
2024-10-14 22:00:03 +1000

    Add TransformComponent

---
+++ Components/TransformComponent.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "../ECS/Component.hpp"
+#include "SFML/System/Vector2.hpp"
+
+class TransformComponent : public Component {
+public:
+    sf::Vector2f position;
+
+    TransformComponent(float x = 0, float y = 0);
+
+    void setPosition(float x, float y);
+    void setPosition(const sf::Vector2f& pos);
+    void translate(float x, float y);
+    void translate(const sf::Vector2f& offset);
+    sf::Vector2f getPosition() const;
+};
