a965b637c7d20ece9109ef7ae86c3657aec1f386
samoilovata <millyhinzpone@gmail.com>
2024-10-15 20:39:21 +1000

    fix previous mistakes

--- Components/TransformComponent.hpp
+++ Components/TransformComponent.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "../ECS/Component.hpp"
-#include "SFML/System/Vector2.hpp"
+#include <SFML/System.hpp>
 
 class TransformComponent : public Component {
 public:
--- ECS/Entity.cpp
+++ ECS/Entity.cpp
@@ -1,12 +1,22 @@
 #include "Entity.hpp"
+#include "../Components/BoundsComponent.hpp"
+#include "../Components/InputComponent.hpp"
+#include "../Components/SpriteComponent.hpp"
+#include "../Components/TransformComponent.hpp"
 
 void Entity::addComponent(std::shared_ptr<Component> ptrComponent) {
     vectorComponent.push_back(ptrComponent);
 }
 
-std::shared_ptr<Component> Entity::getComponent(std::shared_ptr<Component> ptrComponent) {
+template<typename T>
+std::shared_ptr<T> Entity::getComponent() {
     for (auto& component : vectorComponent) {
-        if (component == ptrComponent) return component;
+        if (std::shared_ptr<T> comp = std::dynamic_pointer_cast<T>(component)) return comp;
     }
     return nullptr;
 }
+
+template std::shared_ptr<BoundsComponent> Entity::getComponent<BoundsComponent>();
+template std::shared_ptr<InputComponent> Entity::getComponent<InputComponent>();
+template std::shared_ptr<SpriteComponent> Entity::getComponent<SpriteComponent>();
+template std::shared_ptr<TransformComponent> Entity::getComponent<TransformComponent>();
