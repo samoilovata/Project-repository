ca8842973f624644b573a3bf0189779958a57c88
samoilovata <millyhinzpone@gmail.com>
2024-10-26 16:51:51 +1000

    add new entities instead of old ones

---
+++ Entities/InventoryEntity.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <memory>
+#include "../ECS/Entity.hpp"
+#include "../Components/BoundsComponent.hpp"
+#include "../Components/InputComponent.hpp"
+#include "../Components/SpriteComponent.hpp"
+#include "../Components/TransformComponent.hpp"
+
+class InventoryEntity : public Entity {
+public:
+    void spawnPlayerEntity(const std::string& spritePath, float posX, float posY,
+                           int spriteX, int spriteY, int spriteWidth, int spriteHeight);
+
+private:
+    std::shared_ptr<BoundsComponent> bounds;
+    std::shared_ptr<InputComponent> input;
+    std::shared_ptr<SpriteComponent> sprite;
+    std::shared_ptr<TransformComponent> transform;
+};
---
+++ Entities/ObjectEntity.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+#include "../ECS/Entity.hpp"
+#include "../Components/BoundsComponent.hpp"
+#include "../Components/SpriteComponent.hpp"
+#include "../Components/TransformComponent.hpp"
+
+class ObjectEntity : public Entity {
+public:
+    void spawnObjectEntity(const std::string& spritePath, float posX, float posY,
+                           int spriteX, int spriteY, int spriteWidth, int spriteHeight);
+
+private:
+    std::shared_ptr<BoundsComponent> bounds;
+    std::shared_ptr<SpriteComponent> sprite;
+    std::shared_ptr<TransformComponent> transform;
+};
