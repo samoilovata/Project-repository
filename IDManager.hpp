b5e91e50ca537313fc49a84ac26c84da226ca1ad
Cccuriosity <levshin.iliya@yandex.ru>
2024-11-11 18:15:35 +1000

    Fix IDManager

--- IDManager.hpp
+++ IDManager.hpp
@@ -1,10 +1,4 @@
 #pragma once
-#include <string>
-#include <memory>
-#include "ECS/Entity.hpp"
-#include "Entities/PlayerEntity.hpp"
-#include "Entities/InventoryEntity.hpp"
-#include "Entities/ObjectEntity.hpp"
 
 class IDManager {
     bool getIsRender(int& ID);
