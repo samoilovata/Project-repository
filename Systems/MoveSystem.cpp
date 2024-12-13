ad79cc8bdd50c009817d0a59853eda39c2d402ad
Cccuriosity <levshin.iliya@yandex.ru>
2024-12-13 22:27:56 +1000

    Update moveSystem

--- Systems/MoveSystem.cpp
+++ Systems/MoveSystem.cpp
@@ -4,6 +4,7 @@ void MoveSystem::update(EntityManager& entityManager, sf::Time& deltaTime) {
     for (auto &entity: entityManager.entities) {
         auto transformComponent = entity->getComponent<TransformComponent>();
         auto spriteComponent = entity->getComponent<SpriteComponent>();
+        auto collisionComponent = entity->getComponent<CollisionComponent>();
 
         if (transformComponent && entity->flag == PLAYER) {
             if (frame % 20 == 0) currentFrame++;
@@ -11,22 +12,38 @@ void MoveSystem::update(EntityManager& entityManager, sf::Time& deltaTime) {
 
             if (EntityManager::keyStatus[sf::Keyboard::W]) {
                 spriteComponent->getSprite().setTextureRect(sf::IntRect(int(currentFrame) * 200, 330, 200, 320));
-                transformComponent->translate(sf::Vector2f(0, -4));
+                if (collisionComponent->getCollision() && move.first == 0 && move.second == -1) {
+                    transformComponent->translate(sf::Vector2f(0, 1));
+                }
+                else transformComponent->translate(sf::Vector2f(0, -4));
+                move = {0, -1};
             }
 
             if (EntityManager::keyStatus[sf::Keyboard::S]) {
                 spriteComponent->getSprite().setTextureRect(sf::IntRect(int(currentFrame) * 200, 0, 200, 320));
-                transformComponent->translate(sf::Vector2f(0, 4));
+                if (collisionComponent->getCollision() && move.first == 0 && move.second == 1) {
+                    transformComponent->translate(sf::Vector2f(0, -1));
+                }
+                else transformComponent->translate(sf::Vector2f(0, 4));
+                move = {0, 1};
             }
 
             if (EntityManager::keyStatus[sf::Keyboard::A]) {
                 spriteComponent->getSprite().setTextureRect(sf::IntRect(int(currentFrame) * 200, 980, 200, 320));
-                transformComponent->translate(sf::Vector2f(-4, 0));
+                if (collisionComponent->getCollision() && move.first == -1 && move.second == 0) {
+                    transformComponent->translate(sf::Vector2f(1, 0));
+                }
+                else transformComponent->translate(sf::Vector2f(-4, 0));
+                move = {-1, 0};
             }
 
             if (EntityManager::keyStatus[sf::Keyboard::D]) {
                 spriteComponent->getSprite().setTextureRect(sf::IntRect(int(currentFrame) * 200, 660, 200, 320));
-                transformComponent->translate(sf::Vector2f(4, 0));
+                if (collisionComponent->getCollision() && move.first == 1 && move.second == 0) {
+                    transformComponent->translate(sf::Vector2f(-1, 0));
+                }
+                else transformComponent->translate(sf::Vector2f(4, 0));
+                move = {1, 0};
             }
 
             frame++;
